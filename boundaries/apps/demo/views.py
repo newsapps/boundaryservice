63083d79a9bf7e3c3a3dcd28c3bdef89cc45fdd5
rnagle <rnagle@tribune.com>
2010-12-17 13:51:53 -0600

    adding basic mobile template

--- boundaries/apps/demo/views.py
+++ boundaries/apps/demo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response
 from django.conf import settings
 
+import re
+
 def index(request):
     template_dict = {}
     try:
@@ -10,5 +12,18 @@ def index(request):
         pass
 
     template_dict['domain'] = settings.MY_SITE_DOMAIN
-    
-    return render_to_response('/geo-location.html', template_dict)
+
+    mobile = detect_mobile(request)
+    if mobile:
+        return render_to_response('/geo-location-mobile.html', template_dict)
+    else:
+        return render_to_response('/geo-location.html', template_dict)
+        
+def detect_mobile(request):
+    ua = request.META["HTTP_USER_AGENT"]
+    search = r'mobile'
+    mobile = re.search(search, ua, re.IGNORECASE)
+    if mobile:
+        return True
+    else:
+        return False
