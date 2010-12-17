f7aaaff1c3c3eded19d2a62dff11afc02ca113cf
rnagle <rnagle@tribune.com>
2010-12-16 18:25:52 -0600

    add a simple way of correcting your incorrectly guessed location

--- boundaries/apps/demo/views.py
+++ boundaries/apps/demo/views.py
@@ -3,6 +3,12 @@ from django.conf import settings
 
 def index(request):
     template_dict = {}
+    try:
+        address = request.REQUEST.get('address')
+        template_dict['address'] = address
+    except KeyError:
+        pass
+
     template_dict['domain'] = settings.MY_SITE_DOMAIN
     
     return render_to_response('/geo-location.html', template_dict)
