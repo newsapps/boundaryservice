340bf93e8357987b539705334f961971d3b983e5
Christopher Groskopf <staringmonkey@gmail.com>
2010-12-23 23:56:14 -0600

    Made API version number more sensible. Closes #40.

--- boundaries/apps/api/urls.py
+++ boundaries/apps/api/urls.py
@@ -3,7 +3,7 @@ from tastypie.api import Api
 
 from boundaries.apps.api.resources import BoundarySetResource, BoundaryResource
 
-v1_api = Api(api_name='v1')
+v1_api = Api(api_name='1.0')
 v1_api.register(BoundarySetResource())
 v1_api.register(BoundaryResource())
 
