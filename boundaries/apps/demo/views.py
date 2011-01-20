8041ea046b54b013497ed50c36f585464f143889
rnagle <rnagle@tribune.com>
2011-01-20 15:46:20 -0600

    scrap separate mobile tmpl; use less framework and sass instead

--- boundaries/apps/demo/views.py
+++ boundaries/apps/demo/views.py
@@ -16,19 +16,4 @@ def index(request):
     context['domain'] = settings.MY_SITE_DOMAIN
     context['default_search_text'] = 'Enter an address or drag the pin on the map'
     context['demo_js'] = render_to_string('demo.js', context)
-
-    mobile = detect_mobile(request)
-
-    if mobile:
-        return render_to_response('/geo-location-mobile.html', context)
-    else:
-        return render_to_response('/geo-location.html', context)
-        
-def detect_mobile(request):
-    ua = request.META["HTTP_USER_AGENT"]
-    search = r'mobile'
-    mobile = re.search(search, ua, re.IGNORECASE)
-    if mobile:
-        return True
-    else:
-        return False
+    return render_to_response('/geo-location.html', context)
