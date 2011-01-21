5702a317e145f5c3522b51dd7b3f9d5036599591
cgroskopf <cgroskopf@tribune.com>
2011-01-21 11:07:06 -0600

    Added centroids to boundaries.

---
+++ boundaries/apps/api/migrations/0002_auto__add_field_boundary_centroid.py
@@ -0,0 +1,53 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'Boundary.centroid'
+        db.add_column('api_boundary', 'centroid', self.gf('django.contrib.gis.db.models.fields.PointField')(srid=4269, null=True), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Boundary.centroid'
+        db.delete_column('api_boundary', 'centroid')
+
+
+    models = {
+        'api.boundary': {
+            'Meta': {'ordering': "('kind', 'display_name')", 'object_name': 'Boundary'},
+            'centroid': ('django.contrib.gis.db.models.fields.PointField', [], {'srid': '4269', 'null': 'True'}),
+            'display_name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'external_id': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'kind': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'metadata': ('boundaries.lib.fields.JSONField', [], {'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '192', 'db_index': 'True'}),
+            'set': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'boundaries'", 'to': "orm['api.BoundarySet']"}),
+            'shape': ('django.contrib.gis.db.models.fields.MultiPolygonField', [], {'srid': '4269'}),
+            'simple_shape': ('django.contrib.gis.db.models.fields.MultiPolygonField', [], {'srid': '4269'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '256', 'db_index': 'True'})
+        },
+        'api.boundaryset': {
+            'Meta': {'ordering': "('name',)", 'object_name': 'BoundarySet'},
+            'authority': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'count': ('django.db.models.fields.IntegerField', [], {}),
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'href': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'kind_first': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_updated': ('django.db.models.fields.DateField', [], {}),
+            'metadata_fields': ('boundaries.lib.fields.ListField', [], {'separator': "'|'", 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '64'}),
+            'notes': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'singular': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '256', 'db_index': 'True'})
+        }
+    }
+
+    complete_apps = ['api']
